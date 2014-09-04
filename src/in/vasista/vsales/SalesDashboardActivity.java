54eb2fb802177b6f7dfe8c1b3ae20a09fd997ed9
kvarma <kvarma@50614cf2-9ba8-4bf3-b6ee-b0fb17a9f0db>
2014-09-04 19:04:12 +0000

    change label

--- src/in/vasista/vsales/SalesDashboardActivity.java
+++ src/in/vasista/vsales/SalesDashboardActivity.java
@@ -255,7 +255,7 @@ Log.d(module, "onlySalesDashboard equals " + onlySalesDashboard);
 	{
 	   super.onStop ();  
 	}
-
+  
 /*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
